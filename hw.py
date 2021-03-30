79c225813bd9076e5301d32b13b66dcf2376f805
minseo <kim-west00@naver.com>
2021-03-30 10:43:44 +0900

    완성_TabError뜨긴함

--- hw.py
+++ hw.py
@@ -1,10 +1,11 @@
 
-
-def change_size(self, new_capacity):
-    		print(f'  * changing capacity: {self.capacity} --> {new_capacity}') # 이 첫 문장은 수정하지 말 것
-		B=[0 for i in range(new_capacity)]# 1. new_capacity의 크기의 리스트 B를 만듬
-		for i in range(self.capacity):
-			B[i] = A[i]# 2. self.A의 값을 B로 옮김
-		del self.A  #3.(A 지움)
-		self.A = B
-		self.capacity = new_capacity
+A = [1,2,3,4,5,6]
+capacity = 6
+def change_size(A , capacity, new_capacity):
+    print(f'  * changing capacity: {capacity} --> {new_capacity}') # 이 첫 문장은 수정하지 말 것
+	B = [0 for i in range(new_capacity)]
+	for i in range(capacity):
+		B[i] = A[i]
+	del A  #3.(A 지움)
+	A = B
+	capacity = new_capacity
